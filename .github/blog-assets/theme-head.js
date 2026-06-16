4f0bdcb82d2c49ca5f9d7a64b517982af771a147
Rafael Willians <51998165+Rafaxdz-1000@users.noreply.github.com>
2026-06-16 20:38:42 -0300

    chore(blog-assets): theme-head.js

---
+++ .github/blog-assets/theme-head.js
@@ -0,0 +1,3 @@
+<script id="cdc-theme-head">
+(function(){try{var t=localStorage.getItem('cdc-theme');if(t==='light'){document.documentElement.classList.add('cdc-light');}else if(t==='dark'){document.documentElement.classList.remove('cdc-light');}else if(window.matchMedia&&window.matchMedia('(prefers-color-scheme: light)').matches){document.documentElement.classList.add('cdc-light');}}catch(e){}})();
+</script>
